cee28baf4cb44720a6a65cf5c61b7cc6a1c68cec
Saad Tajwar <saads@ad.unc.edu>
2023-06-18 10:04:48 -0400

    Validation

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     if (!body.name || !body.number) {
         return response.status(400).json({error: 'Need name and number'})
@@ -44,7 +44,7 @@ app.post('/api/persons', (request, response) => {
     const newPerson = new Person({name, number})
     newPerson.save().then(savedPerson => {
         response.json(savedPerson)
-    })
+    }).catch(error => next(error))
 
 })
 
@@ -54,7 +54,7 @@ app.put('/api/persons/:id', (request, response, next) => {
         name: body.name,
         number: body.number
     }
-    Person.findByIdAndUpdate(request.params.id, newPerson, {new: true}).then(updatedPerson => {
+    Person.findByIdAndUpdate(request.params.id, newPerson, {new: true, runValidators: true, context: 'query'}).then(updatedPerson => {
         response.json(updatedPerson)
     }).catch(error => next(error))
 })
@@ -70,7 +70,9 @@ const errorHandler = (error, request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
-    } 
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).send({error: error.message})
+    }
   
     next(error)
 }
