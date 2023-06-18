ebaceb043c2872ea05098868b1d2cb8757c764ed
Saad Tajwar <saads@ad.unc.edu>
2023-06-18 00:42:02 -0400

    Im so fucking tired

--- index.js
+++ index.js
@@ -1,84 +1,47 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Person = require('./models/person')
 app.use(express.json())
 const morgan = require('morgan')
 app.use(morgan('tiny'))
 const cors = require('cors')
 app.use(cors())
 app.use(express.static('build'))
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-app.get('/', (request, response) => {
-    response.json('Working')
-})
-
-app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people <br/>${Date()}`)
-})
+const mongoose = require('mongoose')
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const foundPerson = persons.find(p => p.id === id)
-    if (foundPerson) {
-        response.json(foundPerson)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id).then(person => {
+        response.json(person)
+    })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-    response.status(204).end()
-})
+// app.delete('/api/persons/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     persons = persons.filter(p => p.id !== id)
+//     response.status(204).end()
+// })
 
-const generateId = () => {
-    const maxId = persons.length > 0 ? Math.max(...persons.map(p => p.id)) : 0
-    return maxId + 1
-}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
     if (!body.name || !body.number) {
         return response.status(400).json({error: 'Need name and number'})
-    } else if (persons.find(p => p.name === body.name)) {
-        return response.status(400).json({error: 'Cannot have duplicate names'})
     }
 
-    const newPerson = {
-        name: body.name,
-        number: body.number,
-        id: generateId()
-    }
+    const {name, number} = body
 
-    persons = persons.concat(newPerson)
-    response.json(newPerson)
+    const newPerson = new Person({name, number})
+    newPerson.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+
 })
 
 const unknownEndpoint = (request, response) => {
